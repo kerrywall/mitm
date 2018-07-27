c8e27541b2205871fe341593d091f997965f029b
kerrywall <kerry.wall@gmail.com>
2018-07-27 19:57:40 -0400

    concert page layout

--- single-concert.php
+++ single-concert.php
@@ -12,22 +12,31 @@ get_header();
 
 	<div id="primary" class="content-area">
 		<?php if (has_post_thumbnail()) : ?>
-			<div class="embedded-text">
+			<div class="hero-image">
 				<img class="hero" src="<?php echo the_post_thumbnail_url($large); ?>" alt="">
-				<header class="entry-header">
-					<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
-				</header><!-- .entry-header -->
+				<div class="embedded-text">
+					<header class="entry-header hero">
+						<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
+						<?php if (get_field('date')) { ?>
+							<p class="date"><?php echo get_field('date'); ?></p>
+						<?php } ?>
+					</header><!-- .entry-header -->
+				</div>
 			</div>
+		<?php else : ?>
+			<header class="entry-header no-hero">
+				<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
+				<?php if (get_field('date')) { ?>
+					<p class="date"><?php echo get_field('date'); ?></p>
+				<?php } ?>
+			</header>
 		<?php endif; ?>
 		<main id="main" class="site-main">
-
 		<?php
 		while ( have_posts() ) :
 			the_post();
 
 			get_template_part( 'template-parts/content', get_post_type() );
-
-			the_post_navigation();
 
 		endwhile; // End of the loop.
 		?>
---
+++ template-parts/content-concert.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Template part for displaying page content in page.php
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
+ *
+ * @package musicinthemorning
+ */
+
+?>
+
+<article id="post-<?php the_ID(); ?>" <?php post_class(); ?> class="clearfix">
+
+	<div class="entry-content">
+		<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cupiditate quisquam id, iusto omnis ducimus unde, error, nulla architecto quos expedita fugit laudantium voluptates. Eius quam quibusdam quia animi! Repellat, amet!</p>
+	</div><!-- .entry-content -->
+
+	<div class="entry-sidebar">
+		<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Modi alias totam necessitatibus enim voluptatibus dignissimos repellendus ea, voluptas corporis sunt veniam in earum consequatur adipisci, est. Quis, voluptates. Nisi, autem?</p>
+	</div>
+
+	
+</article><!-- #post-<?php the_ID(); ?> -->
