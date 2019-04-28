20ec71afb16f3e86691c068043409bbfd365afb0
kerrywall <kerry.wall@gmail.com>
2019-04-28 15:11:16 -0400

    changing concert times

--- sidebar.php
+++ sidebar.php
@@ -20,5 +20,5 @@ if ( ! is_active_sidebar( 'sidebar-1' ) ) {
 
 		echo '<h3>Fridays</h3>
 		<p><strong>Christ Church Cathedral</strong><br>
-		Coffee 11am – Concert 11:30am</p>
+		Coffee 10am – Concert 10:30am</p>
 		<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2602.573849970145!2d-123.12252118499624!3d49.284471678565964!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x54867181c0caca91%3A0xd67416bf0b1dc960!2sChrist+Church+Cathedral!5e0!3m2!1sen!2sca!4v1535233871179" width="300" height="200" frameborder="0" style="border:0" allowfullscreen></iframe>'; ?>
