ae847c192de74a9160abae23beab0b27a8615401
kerrywall <kerry.wall@gmail.com>
2019-06-15 18:29:06 -0400

    removing unnecessary wp templating

--- template-parts/content-page-smv.php
+++ template-parts/content-page-smv.php
@@ -62,10 +62,6 @@
 
 		}
 
-		wp_link_pages( array(
-			'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'musicinthemorning' ),
-			'after'  => '</div>',
-		) );
 		?>
 	</div><!-- .entry-content -->
 
